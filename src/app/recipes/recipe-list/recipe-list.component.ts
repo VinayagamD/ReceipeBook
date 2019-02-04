9c1daa267cffcd02cad1f2c60b7e756c83e93e0d
VinayagamD <vinayganeshdxj@gmail.com>
2019-02-04 22:38:46 +0530

    Recipe Setup and Shopping setup

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipe: Recipe[] =
+    [
+      new Recipe('A Taste Recipe', 'This is simply a test', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
+    ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
